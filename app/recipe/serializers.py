70670935d5475c097be950fe01f7b4cd5374597b
Piper <piper.bates@clicktravel.com>
2021-09-13 13:39:45 +0100

    Revert "stripping features for TK onboarding"
    This reverts commit e330a49dd26877b5ce199c8155114bf1a7411d9f.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,38 +1,56 @@
 from rest_framework import serializers
 
-from core.models import Ingredient, Recipe
+from core.models import Tag, Ingredient, Recipe
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for Tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
-    """Serializer for ingredient objects"""
+    """Serializer for Ingredient objects"""
 
     class Meta:
         model = Ingredient
-        fields = ('name',)
+        fields = ('id', 'name')
         read_only_fields = ('id',)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipe objects"""
-    ingredients = IngredientSerializer(many=True)
+    """Serialize a recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
 
-    def create(self, validated_data):
-        ingredients = validated_data.pop('ingredients')
-        recipe = Recipe.objects.create(**validated_data)
-        for ingredient in ingredients:
-            Ingredient.objects.create(recipe=recipe, **ingredient)
-        return recipe
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        Ingredient.objects.filter(recipe=instance.id).delete()
-        ingredients = validated_data.pop('ingredients')
-        for ingredient in ingredients:
-            Ingredient.objects.create(recipe=instance, **ingredient)
-        instance.save()
-        return instance
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
 
     class Meta:
         model = Recipe
-        fields = ('id', 'name', 'description', 'ingredients')
+        fields = ('id', 'title', 'ingredients',
+                  'tags', 'time_minutes',
+                  'price', 'link')
+        read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
         read_only_fields = ('id',)
